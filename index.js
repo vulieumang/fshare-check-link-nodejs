27cd8fcc7e25027d1dc23e79cadb8cc1affef417
Vu Dinh <vulieumang@gmail.com>
2022-04-03 13:13:29 +0700

    change to API

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000;
+const port = 3001;
+// const port = process.env.PORT || 3001;
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // routes will go here
@@ -22,6 +23,11 @@ app.get('/', (req, res) => {
     
 app.post('/', async (req, res) => {
   const link = req.body.link
+  // getInfoFromHTML(link,res)
+  getInfoFromAPI(link, res)
+});
+
+async function getInfoFromHTML(link,res){
   try {
     var data = await getData(link)
     const $ = cheerio.load(data);
@@ -35,14 +41,47 @@ app.post('/', async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-});
+}
+
+async function getInfoFromAPI(link, res){
+  const code_url = link.split('/')[4].split('?')[0]
+  var API_URL = API_URL_Fshare({code:code_url})
+  try {
+    var item = await getData(API_URL)
+    var size = formatBytes(item.current.size)
+    var name = item.current.name
+    var item_obj= {name:name, size:size, link:link}
+    res.render('index', {item:item_obj})
+
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // sv start
 app.listen(port);
+console.log('port')
 console.log('Server started at http://localhost:' + port);
 
 async function getData(url){
   const res = await axios(url)
   const data = await res.data;
   return data
+}
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+function API_URL_Fshare({code, page}) {
+  if(!page){
+    page=1
+  }
+  return `https://www.fshare.vn/api/v3/files/folder?linkcode=${code}&sort=type%2Cname&page=${page}&per-page=500`
 }
--- public/script.js
+++ public/script.js
@@ -3,33 +3,35 @@ var loading = document.querySelector('#loading')
 var res = document.querySelector('#res')
 
 form.onsubmit = function(e) {
-  e.preventDefault();
+  // e.preventDefault();
   loading.style = 'display: block'
-  let formData = new FormData(form);
-  var link = form.querySelector('#link').value
-  link_cor = 'https://proxy1.vulieumang.workers.dev/'+link
-  console.log(link_cor)
-  fetch(link_cor, {
-    method: 'GET',
-    
-  })
-  .then(data => {
-    return data.text();
-  })
-  .then(data => {
-    loading.style = 'display: none'
-    var doc = new DOMParser().parseFromString(data, "text/html");
-    var size = doc.querySelector('#form-download #download-free').innerHTML.split('|')[1].trim()
-    var name = doc.querySelector('.name.text_auto_down').innerText.trim()
-    console.log(name)
-    console.log(size)
-    console.log(link)
-    
-    res.innerHTML= `Tên file: ${name}\nLink: ${link}\nSize: ${size}\nMình cảm ơn`
 
-  })
-  .catch(error => {
-    console.log('Request failed', error);
-  });
+  // checklink pure js
+  // let formData = new FormData(form);
+  // var link = form.querySelector('#link').value
+  // link_cor = 'https://proxy1.vulieumang.workers.dev/'+link
+  // console.log(link_cor)
+  // fetch(link_cor, {
+  //   method: 'GET',
+    
+  // })
+  // .then(data => {
+  //   return data.text();
+  // })
+  // .then(data => {
+  //   loading.style = 'display: none'
+  //   var doc = new DOMParser().parseFromString(data, "text/html");
+  //   var size = doc.querySelector('#form-download #download-free').innerHTML.split('|')[1].trim()
+  //   var name = doc.querySelector('.name.text_auto_down').innerText.trim()
+  //   console.log(name)
+  //   console.log(size)
+  //   console.log(link)
+    
+  //   res.innerHTML= `Tên file: ${name}\nLink: ${link}\nSize: ${size}\nMình cảm ơn`
+
+  // })
+  // .catch(error => {
+  //   console.log('Request failed', error);
+  // });
 }
 
